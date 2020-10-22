7bee92ad843899899eb14a07d679f27d8ef758b1
pomidorkaza <karpov-vb-1996@mail.ru>
2020-10-22 21:02:16 +0300

    no-ver

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -14,23 +14,23 @@ const Rating = ({value, text,color='yellow'}) => {
                  let stars =[
                      ...(()=>{
                          if(fullStarNumber>0){
-                             return new Array(fullStarNumber).fill(0).map(_=><i style={{color}} className='fas fa-star'></i>)
+                             return new Array(fullStarNumber).fill(0).map((_,i)=><i style={{color}} key={`full-${i}`} className='fas fa-star'></i>)
                          } else{
                              return [];
                          }
                      })(),
                     ...(()=>{
                            if(halfStarNumber>=0.5){
-                                return [<i style={{color}} className='fas fa-star-half-alt'></i>]
+                                return [<i key={`half-star`} style={{color}} className='fas fa-star-half-alt'></i>]
                            } else if(halfStarNumber<0.5 &&halfStarNumber>0 ){
-                               return [<i style={{color}} className='fas fa-star'></i>];
+                               return [<i style={{color}} key={`empty-star`} className='fas fa-star'></i>];
                            } else {
                                return [];
                            }
                     })(),
                     ...(()=>{
                         if(emptyStarNumber>0){
-                            return new Array(emptyStarNumber).fill(0).map(_=><i style={{color}}  className='far fa-star'></i>)   
+                            return new Array(emptyStarNumber).fill(0).map((_,i)=><i style={{color}} key={`last-star-${i}`}  className='far fa-star'></i>)   
                         } else {
                             return [];
                         }
@@ -38,7 +38,7 @@ const Rating = ({value, text,color='yellow'}) => {
                 ];
             
                
-              return stars ;
+              return stars;
                })()}
                <span>{text && text}</span>
                 {/* <i className={value >=1 ? 'fas fa-star' : value>=0.5? 'fas fa-star-half-alt':'fas fa-star'}>
